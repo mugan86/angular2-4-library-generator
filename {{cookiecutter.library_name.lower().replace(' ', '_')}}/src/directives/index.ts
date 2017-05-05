48a113680f4a8f9433b44f700557b204d713f189
Anartz Mugika <amugika@codesyntax.com>
2017-05-05 06:47:17 +0200

    Update

--- {{cookiecutter.library_name.lower().replace(' ', '_')}}/src/directives/index.ts
+++ {{cookiecutter.library_name.lower().replace(' ', '_')}}/src/directives/index.ts
@@ -1,2 +1,2 @@
 export {DirectivesModule} from './directives.module';
-export {{{cookiecutter.library_name.capitalize()}}Directive} from './{{cookiecutter.library_name.lower().replace(' ', '_')}}.directive';
+export { {{cookiecutter.library_name.capitalize()}}Directive } from './{{cookiecutter.library_name.lower().replace(' ', '_')}}.directive';

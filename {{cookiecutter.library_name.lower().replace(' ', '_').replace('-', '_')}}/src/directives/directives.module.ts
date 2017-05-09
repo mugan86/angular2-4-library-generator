93f68ef1801298c696f254f8607278c1e9f90daf
Anartz Mugika <amugika@codesyntax.com>
2017-05-09 06:37:01 +0200

    Resolve issue name

--- {{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}/src/directives/directives.module.ts
+++ {{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}/src/directives/directives.module.ts
@@ -1,9 +1,9 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 
-import { {{cookiecutter.library_name.capitalize().replace(' ', '_').replace('-', '_')}}Directive } from './{{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}.directive';
+import { {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '')}}Directive } from './{{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}.directive';
 
 export const DIRECTIVES = [
-  {{cookiecutter.library_name.capitalize().replace(' ', '_').replace('-', '_')}}Directive
+  {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '')}}Directive
 ];
 
 @NgModule({

55cc7b0a8a8d40a9af1969df88d3e99461943d26
Anartz Mugika <amugika@codesyntax.com>
2017-05-09 06:42:00 +0200

    Update

--- {{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}/src/index.ts
+++ {{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}/src/index.ts
@@ -1,3 +1,3 @@
-export { {{cookiecutter.library_name.capitalize().replace('-', '_').replace(' ', '_')}}Module } from "./{{cookiecutter.library_name.lower().replace(' ','_').replace('-', '_')}}.module";
+export { {{cookiecutter.library_name.capitalize().replace('-', '_').replace(' ', '')}}Module } from "./{{cookiecutter.library_name.lower().replace(' ','_').replace('-', '_')}}.module";
 export * from './directives';
 export * from './pipes';

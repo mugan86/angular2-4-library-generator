50abec8c7bb35836c1d0813726d486e3323f4c2c
Anartz Mugika <amugika@codesyntax.com>
2017-06-12 06:12:40 +0200

    Update

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -8,8 +8,10 @@ MODULE_REGEX = r'^[a-zA-Z]{5,10} [A-Za-z]{0,10}$'
 module_name = '{{ cookiecutter.library_name }}'
 
 if not re.match(MODULE_REGEX, module_name):
+    print('*****************************************************************')
     print('ERROR: %s is not a valid angular 2/4 library name!' % module_name)
     print('Please try again and input two words maximum and min 5 characters length one word.')
     print('For example: angular keyboard.')
+    print('*****************************************************************')
     # exits with status 1 to indicate failure
     sys.exit(1)

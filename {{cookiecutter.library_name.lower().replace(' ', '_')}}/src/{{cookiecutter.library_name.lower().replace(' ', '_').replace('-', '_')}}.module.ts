f2390a789588646045931c149b83d20a0708b79d
= <mugan86@gmail.com>
2017-06-14 09:45:49 +0200

    Resolve issue

--- {{cookiecutter.library_name.lower().replace(' ', '_')}}/src/{{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}.module.ts
+++ {{cookiecutter.library_name.lower().replace(' ', '_')}}/src/{{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ModuleWithProviders  } from '@angular/core';
 
 import { PipesModule } from './pipes';
 import { DirectivesModule } from './directives';
-import { ngModule: {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '').replace('_', '')}}Service } from './services/{{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}.service';
+import { {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '').replace('_', '')}}Service } from './services/{{cookiecutter.library_name.lower().replace(' ', '_').replace('-', '_')}}.service';
 
 
 export const MODULES = [
@@ -16,6 +16,6 @@ export const MODULES = [
 })
 export class {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '').replace('_', '')}}Module { 
   static forRoot(): ModuleWithProviders {
-        return {ngModule: {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '').replace('_', '')}}Module, providers: [ngModule: {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '').replace('_', '')}}Service]};
+        return {ngModule: {{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '').replace('_', '')}}Module, providers: [{{cookiecutter.library_name.capitalize().replace('-', '').replace(' ', '').replace('_', '')}}Service]};
     }
 }
